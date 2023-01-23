8f2259427c328372791cb84fa675c2216375df4a
ZonaGuillermo <zonaguillermoweb@gmail.com>
2023-01-23 13:28:40 +0100

    Installation, setup & colors

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        vuejs: {
+          100: "#49e659",
+          932: "#434252"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
