04ff29d9020f731c9a866a8a0fa9487eda8ed6d2
ZonaGuillermo <zonaguillermoweb@gmail.com>
2023-01-24 12:55:05 +0100

    Spaces & Sizes concepts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,22 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl italic ">Title 1</h1>
-      <h2 className="text-2xl">Title 2</h2>
-      <h3 className="text-xl">Title 3</h3>
-      <p className="text-base underline underline-offset-4 decoration-green-600 decoration-1 decoration-wavy">A regular paragraph</p>
-      <p className="text-sm">A description paragraph</p>
-      <p className="note text-xs">A little note</p>
-      <div style= {{ width: '200px'}}>
-        <p className="truncate">This is a long sentence with a lot of words to explain that truncate will truncate my sentence</p>
+      <div className="m-4 p-2 pl-4 font-bold text-white bg-red-500">
+        Hello
+      </div>
+
+        <div className="text-white font-bold m-2 w-40 h-40 bg-red-500">1</div>
+        <div className="text-white font-bold m-8 -mt-8 w-20 h-20 bg-green-500 opacity-75">2</div>
+    
+
+      {/* NOT A GOOD PRACTISE for to get this behavior you have to use gap utilities */}
+      <div className="flow-root">
+        <p>NOT A GOOD PRACTISE for to get this behavior you have to use gap utilities</p>
+        <div className="text-white font-bold -m-2 flex flex-wrap">
+          <div className="text-white font-bold m-2 w-40 h-40 bg-red-500">1</div>
+          <div className="text-white font-bold m-2 w-40 h-40 bg-green-500">2</div>
+          <div className="text-white font-bold m-2 w-40 h-40 bg-blue-500">3</div>
+        </div>
       </div>
     </div>
   )
