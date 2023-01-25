a16f521423e88889a3cc504a05083d73128098e2
ZonaGuillermo <zonaguillermoweb@gmail.com>
2023-01-25 13:23:31 +0100

    Layout Concepts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,102 @@
 export default function Home() {
-  return (
-    <>
-      <div className="text-white Parent bg-slate-900">
-        <div className="container mx-auto">
-          <div className="grid grid-flow-col grid-rows-4 gap-4">
-            <div className="row-span-3 p-6 rounded-lg bg-sky-500">01</div>
-            <div className="p-6 rounded-lg bg-sky-500">02</div>
-            <div className="p-6 rounded-lg bg-sky-500">03</div>
-            <div className="p-6 rounded-lg bg-sky-500">04</div>
-            <div className="row-span-3 p-6 rounded-lg bg-sky-500">05</div>
-            <div className="p-6 rounded-lg bg-sky-500">06</div>
-            <div className="p-6 rounded-lg bg-sky-500">07</div>
-            <div className="p-6 rounded-lg bg-sky-500">08</div>
-            <div className="p-6 rounded-lg bg-sky-500">09</div>
-            <div className="p-6 rounded-lg bg-sky-500">10</div>
-            <div className="p-6 rounded-lg bg-sky-500">11</div>
-            <div className="p-6 rounded-lg bg-sky-500">12</div>
-          </div>
-        </div>
-      </div>
+	return (
+		<>
+			
+			{/* 
+					We can center the container class by default indicating it in 
+					tailwind.config.js file in theme section:
+					
+						module.exports = {
+							theme: {
+								container: {
+									center: true,
+								},
+							},
+						}
 
-      <br />
-      <hr />
-      <br />
+					This way we don't need to use mx-auto class all the time 
+			*/}
 
-      <div className="text-white Parent bg-slate-900">
-        <div className="container mx-auto">
-          <div className="grid grid-flow-col-dense grid-rows-4 gap-4">
-            <div className="row-span-3 p-6 rounded-lg bg-sky-500">01</div>
-            <div className="p-6 rounded-lg bg-sky-500">02</div>
-            <div className="p-6 rounded-lg bg-sky-500">03</div>
-            <div className="p-6 rounded-lg bg-sky-500">04</div>
-            <div className="row-span-3 p-6 rounded-lg bg-sky-500">05</div>
-            <div className="p-6 rounded-lg bg-sky-500">06</div>
-            <div className="p-6 rounded-lg bg-sky-500">07</div>
-            <div className="p-6 rounded-lg bg-sky-500">08</div>
-            <div className="p-6 rounded-lg bg-sky-500">09</div>
-            <div className="p-6 rounded-lg bg-sky-500">10</div>
-            <div className="p-6 rounded-lg bg-sky-500">11</div>
-            <div className="p-6 rounded-lg bg-sky-500">12</div>
-          </div>
-        </div>
-      </div>
+			<div className="text-white parent bg-slate-900">
+				<div className="container mx-auto p-2 bg-red-500">
+					<h1>Hello</h1>
+				</div>
+			</div>
 
-    </>
-  )
+			<br />
+			<hr />
+			<br />
+
+			<div className="text-white parent bg-slate-900">
+				<div className="container mx-auto p-2 columns-sm">
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+				</div>
+			</div>
+
+			<br />
+			<hr />
+			<br />
+
+			<div className="text-white parent bg-slate-900">
+				<div className="container mx-auto p-2 columns-sm">
+					<img className="w-64 float-left" src="https://images.unsplash.com/photo-1536853340577-a1e6343d8a61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=674&q=80" alt="from unsplash" />
+
+					<img className="w-32 float-right" src="https://images.unsplash.com/photo-1536853340577-a1e6343d8a61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=674&q=80" alt="from unsplash" />
+
+					<p className="clear-right">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+				</div>
+			</div>
+
+			<br />
+			<hr />
+			<br />
+
+			{/**
+			 * ? The second image should be in the right side of the last column... 
+			 * ? How I can fix it?
+			 */}
+
+			<div className="text-white parent bg-slate-900">
+				<div className="container mx-auto p-2 columns-sm relative">
+					<img className="w-64 float-left" src="https://images.unsplash.com/photo-1536853340577-a1e6343d8a61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=674&q=80" alt="from unsplash" />
+
+					<img className="w-32 float-right static right-0" src="https://images.unsplash.com/photo-1536853340577-a1e6343d8a61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=674&q=80" alt="from unsplash" />
+
+					<p className="clear-right">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+				</div>
+			</div>
+
+			<br />
+			<hr />
+			<br />
+
+			<div className="text-white parent bg-slate-900">
+				<div className="container px-2">
+					<div className="relative bg-red-500 rounded-lg p-8 w-32 h-32">
+						<div className="w-24 h-24 bg-green-500 rounded-lg absolute inset-2">
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
