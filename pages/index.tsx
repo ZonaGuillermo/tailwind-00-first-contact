494569f164908c3d13c612070cfd00a3e2d3cc69
ZonaGuillermo <zonaguillermoweb@gmail.com>
2023-01-25 14:12:25 +0100

    Border, divide & ring concepts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,19 @@
 export default function Home() {
 	return (
 		<>
-			
-			{/* 
-					We can center the container class by default indicating it in 
-					tailwind.config.js file in theme section:
-					
-						module.exports = {
-							theme: {
-								container: {
-									center: true,
-								},
-							},
-						}
+			<div className="text-white bg-slate-900 parent">
+				<header className="w-full border-b border-l-8 p-2 font-bold">Hello</header>
+			</div>
 
-					This way we don't need to use mx-auto class all the time 
-			*/}
+			<br />
+			<hr />
+			<br />
 
-			<div className="text-white parent bg-slate-900">
-				<div className="container mx-auto p-2 bg-red-500">
-					<h1>Hello</h1>
+			<div className="text-white bg-slate-900 parent">
+				<div className="divide-y divide-slate-200">
+					<div>01</div>
+					<div>02</div>
+					<div>03</div>
 				</div>
 			</div>
 
@@ -27,15 +21,19 @@ export default function Home() {
 			<hr />
 			<br />
 
-			<div className="text-white parent bg-slate-900">
-				<div className="container mx-auto p-2 columns-sm">
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
+			<div className="text-white bg-slate-900 parent">
+				<input type="text" className="border-2 border-dashed outline-none border-rose-600" />
+			</div>
+
+			<br />
+			<hr />
+			<br />
+
+			<div className="text-white bg-slate-900 parent">
+				<div className="grid grid-cols-3 divide-x-2 divide-red-500 divide-dashed">
+					<div className="px-2">01</div>
+					<div className="px-2">02</div>
+					<div className="px-2">03</div>
 				</div>
 			</div>
 
@@ -43,59 +41,8 @@ export default function Home() {
 			<hr />
 			<br />
 
-			<div className="text-white parent bg-slate-900">
-				<div className="container mx-auto p-2 columns-sm">
-					<img className="w-64 float-left" src="https://images.unsplash.com/photo-1536853340577-a1e6343d8a61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=674&q=80" alt="from unsplash" />
-
-					<img className="w-32 float-right" src="https://images.unsplash.com/photo-1536853340577-a1e6343d8a61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=674&q=80" alt="from unsplash" />
-
-					<p className="clear-right">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-				</div>
-			</div>
-
-			<br />
-			<hr />
-			<br />
-
-			{/**
-			 * ? The second image should be in the right side of the last column... 
-			 * ? How I can fix it?
-			 */}
-
-			<div className="text-white parent bg-slate-900">
-				<div className="container mx-auto p-2 columns-sm relative">
-					<img className="w-64 float-left" src="https://images.unsplash.com/photo-1536853340577-a1e6343d8a61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=674&q=80" alt="from unsplash" />
-
-					<img className="w-32 float-right static right-0" src="https://images.unsplash.com/photo-1536853340577-a1e6343d8a61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=674&q=80" alt="from unsplash" />
-
-					<p className="clear-right">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nemo eveniet ratione, natus quod voluptatem aliquid voluptatum vitae, amet beatae harum quae non earum! Quasi neque nesciunt nobis soluta laboriosam?</p>
-				</div>
-			</div>
-
-			<br />
-			<hr />
-			<br />
-
-			<div className="text-white parent bg-slate-900">
-				<div className="container px-2">
-					<div className="relative bg-red-500 rounded-lg p-8 w-32 h-32">
-						<div className="w-24 h-24 bg-green-500 rounded-lg absolute inset-2">
-
-						</div>
-					</div>
-				</div>
+			<div className="text-white bg-slate-900 parent">
+				<button className="outline outline-offset-2 outline-4 outline-rose-500 rounded-sm hover:ring-8 hover:ring-red-700 ">Button C</button>
 			</div>
 		</>
 	)
