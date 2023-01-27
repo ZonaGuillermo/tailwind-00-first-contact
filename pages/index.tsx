ffd8419b2dd75601bd4f3d42cb767a4dcbd91512
ZonaGuillermo <zonaguillermoweb@gmail.com>
2023-01-27 10:02:21 +0100

    Effects & Filters concepts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,13 @@
 export default function Home() {
 	return (
 		<>
-			<div className="text-white bg-slate-900 parent">
-				<header className="w-full border-b border-l-8 p-2 font-bold">Hello</header>
-			</div>
-
-			<br />
-			<hr />
-			<br />
-
-			<div className="text-white bg-slate-900 parent">
-				<div className="divide-y divide-slate-200">
-					<div>01</div>
-					<div>02</div>
-					<div>03</div>
+			<div className="parent p-4">
+				<div className="inline-block p-4 text-black bg-white border rounded-lg shadow-inner">
+					<h1 className="text-2xl">Hello</h1>
+					<p className="mb-4">This is my card</p>
+					<button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-indigo-500/4 opacity-25">
+						Say Hello
+					</button>
 				</div>
 			</div>
 
@@ -21,19 +15,13 @@ export default function Home() {
 			<hr />
 			<br />
 
-			<div className="text-white bg-slate-900 parent">
-				<input type="text" className="border-2 border-dashed outline-none border-rose-600" />
-			</div>
-
-			<br />
-			<hr />
-			<br />
-
-			<div className="text-white bg-slate-900 parent">
-				<div className="grid grid-cols-3 divide-x-2 divide-red-500 divide-dashed">
-					<div className="px-2">01</div>
-					<div className="px-2">02</div>
-					<div className="px-2">03</div>
+			<div className="parent p-4">
+				<div className="inline-block p-4 text-black bg-white border rounded-lg shadow-inner blur hover:blur-none transition">
+					<h1 className="text-2xl">Hello</h1>
+					<p className="mb-4">This is my card</p>
+					<button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-indigo-500/4 opacity-25">
+						Say Hello
+					</button>
 				</div>
 			</div>
 
@@ -41,8 +29,18 @@ export default function Home() {
 			<hr />
 			<br />
 
-			<div className="text-white bg-slate-900 parent">
-				<button className="outline outline-offset-2 outline-4 outline-rose-500 rounded-sm hover:ring-8 hover:ring-red-700 ">Button C</button>
+			<div className="parent p-4">
+				<div className="inline-block p-4 text-black bg-white border rounded-lg shadow-inner">
+					<img
+						src="https://images.unsplash.com/photo-1508144753681-9986d4df99b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="from unsplash"
+						className="w-64 mb-4 brightness-125 hover:contrast-50"
+					/>
+					<h1 className="text-2xl">Hello</h1>
+					<p className="mb-4">This is my card</p>
+					<button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-indigo-500/4 opacity-25">
+						Say Hello
+					</button>
+				</div>
 			</div>
 		</>
 	)
